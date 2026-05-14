bebbf7b597255f9f46007dc16a2370b8cdee01e6
Kashan Shah <kashanshah@hotmail.com>
2026-05-14 12:29:54 -0400

    Add SSR configuration to Vite setup, specifying noExternal for @fancyapps/ui

--- vite.ssr.config.ts
+++ vite.ssr.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  ssr: {
+    noExternal: ["@fancyapps/ui"],
+  },
   build: {
     ssr: "src/entry-server.tsx",
     outDir: "dist/.prerender-server",
